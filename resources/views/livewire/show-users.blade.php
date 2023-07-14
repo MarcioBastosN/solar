e34a6b428963fe5238369453382d279beb7fe4c3
MarcioBastos <marciobastosn@gmail.com>
2023-07-14 15:19:40 -0300

    .

--- resources/views/livewire/show-users.blade.php
+++ resources/views/livewire/show-users.blade.php
@@ -1,7 +1,12 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
     <div class="max-w-lg mx-auto ">
-        <input wire:model="search" type="search" placeholder="Procura email..." class="bg-primary-200 bg-opacity-70 ">
+        <input wire:model="search" type="search" placeholder="Procura email..."
+            class="mt-2 block rounded-t-lg px-2.5 pb-1.5 pt-5 w-full
+        text-sm text-gray-900 bg-primary-100 dark:bg-gray-700 border-0
+        border-b-2 border-gray-300 appearance-none dark:text-white
+        dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none
+        focus:ring-0 focus:border-blue-600 peer">
         <p>Lista de clientes registrados</p>
     </div>
 
