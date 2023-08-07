4e6196db5328158fcec266336d35237d87a1c7af
MarcioBastos <marciobastosn@gmail.com>
2023-08-07 13:18:57 -0300

    ajustes view celular

--- resources/views/livewire/show-users.blade.php
+++ resources/views/livewire/show-users.blade.php
@@ -7,7 +7,6 @@
         border-b-2 border-gray-300 appearance-none dark:text-white
         dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none
         focus:ring-0 focus:border-blue-600 peer">
-        {{-- <p>Lista de clientes registrados</p> --}}
     </div>
 
     @foreach ($users as $user)
