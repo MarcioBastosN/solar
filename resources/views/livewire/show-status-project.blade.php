fddd1b65249986e3efeea914be09d641107ed20c
MarcioBastos <marciobastosn@gmail.com>
2023-08-07 13:23:30 -0300

    ajuste view celular

--- resources/views/livewire/show-status-project.blade.php
+++ resources/views/livewire/show-status-project.blade.php
@@ -1,19 +1,26 @@
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
-    <div class="grid grid-cols-2 my-2 mx-4">
-        <div class="mx-4">
-            <p><b>Status possiveis do Projeto</b></p>
-            <hr>
-            @foreach ($statusProject as $status)
-                <p>{{ $status->label }}</p>
-            @endforeach
+    <section class="bg-white ">
+        <div class="container w-full mx-auto">
+            <div class="flex flex-wrap">
+                <div class="w-full sm:w-1/2">
+                    <div class="mx-4 my-2">
+                        <p><b>Status possiveis do Projeto</b></p>
+                        <hr>
+                        @foreach ($statusProject as $status)
+                            <p>{{ $status->label }}</p>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="w-full sm:w-1/2">
+                    <div class="mx-4 my-2">
+                        <p><b>Status possiveis dos Documentos</b></p>
+                        <hr>
+                        @foreach ($statusDocumentos as $status)
+                            <p>{{ $status->label }}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="mx-4">
-            <p><b>Status possiveis dos Documentos</b></p>
-            <hr>
-            @foreach ($statusDocumentos as $status)
-                <p>{{ $status->label }}</p>
-            @endforeach
-        </div>
-    </div>
+    </section>
 </div>
