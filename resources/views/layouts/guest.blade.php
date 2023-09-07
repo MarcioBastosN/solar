0e63ee50469e40fc1cf2ef6cbcfe48de2c1677c8
Marcio Andre Neves Bastos <marciobastosn@gmail.com>
2023-09-07 20:23:00 -0300

    comentado alpine

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     @wireUiScripts
-    <script src="//unpkg.com/alpinejs" defer></script>
+    {{-- <script src="//unpkg.com/alpinejs" defer></script> --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Scripts -->
     @livewireStyles
