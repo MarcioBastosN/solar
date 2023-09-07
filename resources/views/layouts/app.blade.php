9f59dc2be48405eb9663c732f8f4b74af7243355
Marcio Andre Neves Bastos <marciobastosn@gmail.com>
2023-09-07 19:49:51 -0300

    orden script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     {{-- <script src="../path/to/flowbite/dist/flowbite.min.js"></script> --}}
-    <script src="//unpkg.com/alpinejs" defer></script>
     @wireUiScripts
+    <script src="//unpkg.com/alpinejs" defer></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Scripts -->
     @livewireStyles
