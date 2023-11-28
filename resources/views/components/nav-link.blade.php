d42026edcde1917dd38e92875a4b6f7b6be8693d
MarcioBastosN <marciobastosn@gmail.com>
2023-11-28 09:15:20 -0300

    ajustes menu

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -5,11 +5,11 @@
         $active ?? false
             ? 'inline-flex items-center px-1 pt-1
             border-b-2 border-secondary dark:border-secondary
-            text-sm font-medium leading-5 text-secondary dark:text-secodary
+            text-sm font-medium leading-5 text-white dark:text-secodary
             focus:outline-none focus:border-primary
             transition duration-150 ease-in-out'
             : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent
-            text-sm font-medium leading-5 text-gray-500 hover-text-secondary
+            text-sm font-medium leading-5 text-gray-400 hover-text-secondary
             hover:border-gray-700 focus:outline-none focus:text-secondary
             focus:border-secondary transition duration-150 ease-in-out';
 @endphp
--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -4,12 +4,12 @@
     $classes =
         $active ?? false
             ? 'block w-fullpl-3 pr-4 py-2 border-l-4 border-secondary text-left text-base font-medium
-            dark:text-secondary text-secondary bg-primary
+            dark:text-secondary text-white bg-primary
             focus:outline-none focus:text-primary
             focus:bg-primary focus:border-primary
             transition duration-150 ease-in-out'
             : 'block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium
-            dark:text-secondary  text-secondary hover:text-gray-600 hover:bg-gray-50 hover:border-gray-300
+            dark:text-secondary  text-white hover:text-gray-600 hover:bg-gray-50 hover:border-gray-300
             focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300
             transition duration-150 ease-in-out';
 @endphp
